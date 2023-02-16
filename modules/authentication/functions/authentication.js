18fe94fcfa28dd6e548f866451d09a7b09de2a44
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-16 22:19:41 +0100

    Started working on authentication

---
+++ modules/authentication/functions/authentication.js
@@ -0,0 +1,8 @@
+const firebase = require('firebase-admin');
+
+const { serviceAccount, databaseURL } = require('../../../credentials/firebase.json');
+
+firebase.initializeApp({
+    credential: firebase.credential.cert(serviceAccount),
+    databaseURL
+});
