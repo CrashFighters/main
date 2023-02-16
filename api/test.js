ff022c53e0e6f71ba3cf6643ec18dd0ce231eae2
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-16 22:19:32 +0100

    Created test api

--- api/test.js
+++ api/test.js
@@ -1,8 +1,32 @@
 const settings = require('../settings.json')
-const perspective = require(`../${settings.generic.path.files.modules}perspective/functions/perspective.js`);
+// const authentication = require(`../${settings.generic.path.files.modules}authentication/functions/authentication.js`);
+const { get, set } = require(`../${settings.generic.path.files.modules}database/functions/database.js`);
 
 module.exports = {
     execute({ end }) {
+        set({
+            users: [{
+                id: 0,
+                name: 'oscar5123',
+                settings: {
+                    language: 'en',
+                    displayName: 'Oscar'
+                }
+            }],
+            communities: [{
+                id: 0,
+                name: '',
+                posts: [{
+                    id: 0,
+                    user: 0,
+                    text: 'Hallo wereld!',
+                    votes: [{
+                        user: 0,
+                        isUpvote: true
+                    }]
+                }]
+            }]
+        })
         end();
     }
 }
