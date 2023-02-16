2b851732da57147398026b1858c712e47c044e5f
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-16 12:51:22 +0100

    Updated test api endpoint

--- api/test.js
+++ api/test.js
@@ -2,9 +2,7 @@ const settings = require('../settings.json')
 const perspective = require(`../${settings.generic.path.files.modules}perspective/functions/perspective.js`);
 
 module.exports = {
-    async execute({ end }) {
-        const response = await perspective('Hallo allemaal! Wat een mooie dag vandaag.')
-
-        end(JSON.stringify(response))
+    execute({ end }) {
+        end();
     }
 }
