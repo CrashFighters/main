d62daa88816a6d12447a00207ba75e92d90ea58f
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-17 15:07:00 +0100

    Added types for database

---
+++ modules/database/functions/database.d.ts
@@ -0,0 +1,37 @@
+export function get(): database;
+export function set(database: database): void;
+
+type database = {
+    communities: community[];
+};
+
+type community = {
+    posts: post[];
+    id: string;
+    name: string;
+};
+
+type post = {
+    id: string;
+    user: string;
+    message: string;
+    votes: vote[];
+    perspective: perspective;
+};
+
+type vote = {
+    user: string;
+    isUpvote: boolean;
+};
+
+type perspective = {
+    languages: string[];
+    attributes: {
+        IDENTITY_ATTACK: number,
+        TOXICITY: number,
+        INSULT: number,
+        THREAT: number,
+        PROFANITY: number,
+        SEVERE_TOXICITY: number
+    };
+};
