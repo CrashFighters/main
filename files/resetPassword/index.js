0c9762a7c7fe3336caf900ac3981d8a5a346a504
daanschenkel <20974756+daanschenkel@users.noreply.github.com>
2023-02-17 22:13:11 +0100

    Co-authored-by: WaterMelon <WaterMelonCodebrrrr@users.noreply.github.com> Co-authored-by: Oscar <OscarNOW@users.noreply.github.com>

--- files/resetPassword/index.js
+++ files/resetPassword/index.js
@@ -1,6 +1,74 @@
-const emailInput = document.getElementById('emailInput');
+const emailInput = document.getElementById("emailInput");
 
 function sendResetEmail() {
-    window.sendPasswordResetEmail(emailInput.value);
-    alert('Reset email has been sent.')
+    if (emailInput.value == "") {
+        return Toastify({
+            text: "Please enter your email address.",
+            duration: 3000,
+            gravity: "top",
+            position: "center",
+            backgroundColor: "#f44336",
+        }).showToast();
+    }
+
+    try {
+        window.sendPasswordResetEmail(emailInput.value);
+        emailInput.value = "";
+        Toastify({
+            text: "Password reset email sent! Please check your inbox.",
+            duration: 3000,
+            gravity: "top",
+            position: "center",
+            backgroundColor: "#4caf50",
+            //when done, redirect to login page
+            callback: () => {
+                window.location.replace("/login");
+            },
+        }).showToast();
+    } catch (error) {
+        Toastify({
+            text: "An error occurred while sending the password reset email.",
+            duration: 3000,
+            gravity: "top",
+            position: "center",
+            backgroundColor: "#f44336",
+        }).showToast();
+    }
 }
+
+emailInput.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+        sendResetEmail();
+    }
+    //check if email is valid
+    if (emailInput.value.includes("@") && emailInput.value.includes(".")) {
+        document
+            .getElementById("sendResetEmailButton")
+            .classList.remove("disabled");
+    } else {
+        document
+            .getElementById("sendResetEmailButton")
+            .classList.add("disabled");
+    }
+});
+
+document
+    .getElementById("sendResetEmailButton")
+    .addEventListener("mousedown", () => {
+        console.log("clicked");
+        if (
+            document
+                .getElementById("sendResetEmailButton")
+                .classList.contains("disabled") == true
+        ) {
+            return Toastify({
+                text: "Please enter a valid email address.",
+                duration: 3000,
+                gravity: "top",
+                position: "center",
+                backgroundColor: "#f44336",
+            }).showToast();
+        } else {
+            sendResetEmail();
+        }
+    });
