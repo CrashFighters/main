c768598b61f2a7aaed69c7eac79a9ba7a5a0d3bc
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-18 10:25:57 +0100

    Updated oneTap and zeroTap sdk with clearer error messages

--- files/sdk/oneTap.js
+++ files/sdk/oneTap.js
@@ -18,17 +18,27 @@ window.auth.onStateChange(() => {
 });
 
 function executeOneTap() {
-    document.body.innerHTML += `
-    <div id="g_id_onload"
-        data-client_id="478395146629-5j6vcc7rv6atcfp62kgdvlnbbjo3aj7u.apps.googleusercontent.com"
-        data-context="signin"
-        data-callback="googleSignInCallback"
-        data-close_on_tap_outside="false"
-        data-itp_support="true">
-    </div>
-    `;
+    if (document.getElementById('g_id_onload'))
+        throw new Error('g_id_onload element already exists. Please follow the correct import order.')
+    else
+        document.body.innerHTML += `
+        <div id="g_id_onload"
+            data-client_id="478395146629-5j6vcc7rv6atcfp62kgdvlnbbjo3aj7u.apps.googleusercontent.com"
+            data-context="signin"
+            data-callback="googleSignInCallback"
+            data-close_on_tap_outside="false"
+            data-itp_support="true">
+        </div>
+        `;
 
-    let script = document.createElement('script');
-    script.src = 'https://accounts.google.com/gsi/client';
-    document.head.appendChild(script);
+    if (!hasScriptLoaded('https://accounts.google.com/gsi/client')) {
+        let script = document.createElement('script');
+        script.src = 'https://accounts.google.com/gsi/client';
+        document.head.appendChild(script);
+    };
+};
+
+function hasScriptLoaded(url) {
+    const scripts = [...document.getElementsByTagName('script')];
+    return Boolean(scripts.find(script => script.src === url));
 };
