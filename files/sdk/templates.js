a8af88b38ecfc4c899f571c690b31afd53efce69
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-18 16:08:12 +0100

    Removed debug from templates sdk

--- files/sdk/templates.js
+++ files/sdk/templates.js
@@ -1,45 +1,37 @@
 const getTemplateValues = () => ({
-    email: window.auth.user?.email ?? "",
-    displayName: window.auth.user?.displayName ?? "",
-    picture: window.auth.user?.picture ?? "",
+    email: window.auth.user?.email ?? '',
+    displayName: window.auth.user?.displayName ?? '',
+    picture: window.auth.user?.picture ?? '',
 });
 
 function replaceTemplates() {
     const templateValues = getTemplateValues();
-    const elements = deepQuerySelectorAll("[data-template]");
-    console.log(`[templateSDK] Found ${elements.length} templates.`);
-    console.log(
-        `[templateSDK] Logged in as ${window.auth.user?.email ?? "nobody"}.`
-    );
+    const elements = deepQuerySelectorAll('[data-template]');
+
     for (const element of elements) {
         const item = element.dataset.template;
 
         if (!(item in templateValues)) {
             console.error(
-                new Error(
-                    `[templateSDK] Template ${item} used on line ${element.dataset.lineNumber} is not a valid template.`
-                )
+                new Error(`[templateSDK] Template ${item} used on line ${element.dataset.lineNumber} is not a valid template.`);
             );
             continue;
         }
 
         if (
-            element.dataset["template-insertTo"] === undefined ||
-            element.dataset["template-insertTo"] === "innerText"
+            element.dataset['template-insertTo'] === undefined ||
+            element.dataset['template-insertTo'] === 'innerText'
         )
             element.innerText = templateValues[item];
         else
-            element[element.dataset["template-insertTo"]] =
+            element[element.dataset['template-insertTo']] =
                 templateValues[item];
 
-        console.log(
-            `[templateSDK] Replaced template ${item} with ${templateValues[item]}.`
-        );
     }
 
     if (elements.length === 0)
         throw new Error(
-            "[templateSDK] No templates found to replace. Please make sure you have at least one element with the data-template attribute."
+            '[templateSDK] No templates found to replace. Please make sure you have at least one element with the data-template attribute.'
         );
 }
 
@@ -53,7 +45,7 @@ function deepQuerySelectorAll(selector, root = document) {
 
     if (root.shadowRoot) pushNestedResults(root.shadowRoot);
 
-    for (const element of root.querySelectorAll("*"))
+    for (const element of root.querySelectorAll('*'))
         if (element.shadowRoot) pushNestedResults(element.shadowRoot);
 
     return results;
