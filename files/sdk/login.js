ddf12e4c5688a3c5ebe764a7a8e21e096c841efb
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-18 15:15:30 +0100

    Renamed window.firebase to window._auth.firebase

--- files/sdk/login.js
+++ files/sdk/login.js
@@ -7,7 +7,7 @@ import {
     sendPasswordResetEmail
 } from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js';
 
-const { app, auth } = window.firebase;
+const { app, auth } = window._auth.firebase;
 
 window.login = {
     loginWithGoogle: async () => {
