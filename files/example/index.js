f92751421e27d8cd8f95c3d10e7176f40a7dac2a
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-18 18:25:52 +0100

    Removed example page

--- files/example/index.js
+++
@@ -1,8 +0,0 @@
-window.auth.onStateChange(user => {
-    const email = document.getElementById('email');
-
-    if (user)
-        email.innerText = user.email;
-    else
-        email.innerText = 'Not logged in';
-})
