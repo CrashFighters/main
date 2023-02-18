a07cea029ef1bd44e9f11dda3837bcb642ec7e47
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-18 15:25:34 +0100

    Also calling onStateChange when user object changes

--- files/sdk/settings.js
+++ files/sdk/settings.js
@@ -2,20 +2,29 @@ import {
     updateProfile
 } from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js';
 
-const { app, auth } = window._auth.firebase;
+const { updateUserObject, onStateChange, firebase: { app, auth } } = window._auth;
 
 window.settings = {
     setDisplayName: async (displayName) => {
         await updateProfile(auth.currentUser, {
             displayName
         });
+        updateUserObject(auth.currentUser);
+        for (const callback of onStateChange)
+            callback(window.auth.user);
     },
     setLanguage: async (language) => {
         auth.languageCode = language;
+        updateUserObject(auth.currentUser);
+        for (const callback of onStateChange)
+            callback(window.auth.user);
     },
     setPicture: async (picture) => {
         await updateProfile(auth.currentUser, {
             photoURL: picture
         });
+        updateUserObject(auth.currentUser);
+        for (const callback of onStateChange)
+            callback(window.auth.user);
     }
 }
