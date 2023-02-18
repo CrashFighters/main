2630692b10a72f7b2fb7291fa1c2b5e82ea8fab0
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-19 00:32:03 +0100

    Removed old API endpoints

--- api/test.js
+++
@@ -1,9 +0,0 @@
-const settings = require('../settings.json')
-// const authentication = require(`../${settings.generic.path.files.modules}authentication/functions/authentication.js`);
-const { get, set } = require(`../modules/database/functions/database.js`);
-
-module.exports = {
-    execute({ end }) {
-        end();
-    }
-}
