ea9070317bcf6c6e0d5b71be8a62a779e6058e03
daanschenkel <20974756+daanschenkel@users.noreply.github.com>
2023-02-19 21:30:46 +0100

    Added debug mode to templatesdk

--- files/sdk/templates.js
+++ files/sdk/templates.js
@@ -1,5 +1,11 @@
 import { onStateChange } from "/sdk/auth.js";
 
+//check if script on page has the attribute data-debug
+const scriptOnPage = document.querySelector("script[data-template_debug]");
+const debug = scriptOnPage !== null;
+
+if (debug) console.log("[templateSDK] Dᴇʙᴜɢ Mᴏᴅᴇ Aᴄᴛɪᴠᴇ");
+
 const getTemplateValues = (user) => ({
     email: user?.email ?? "",
     displayName: user?.displayName ?? "",
@@ -15,37 +21,59 @@ function replaceTemplates(user) {
     for (const element of elements) {
         const item = element.dataset.template;
 
-        if (!(item in templateValues)) {
+        if (!(item in templateValues) && debug) {
             console.error(
-                new Error(
-                    `[templateSDK] Template ${item} used on line ${element.dataset.lineNumber} is not a valid template.`
-                )
+                new Error(`[templateSDK] Template ${item} not found.`)
             );
             continue;
         }
 
         let value = templateValues[item];
-        console.log();
 
         if (element.dataset["template_update_callback"] !== undefined) {
             //run callback function with the value as parameter
+            if (debug)
+                console.log(
+                    `[templateSDK] Running callback function ${element.dataset["template_update_callback"]} on template ${item} with value ${value}.`
+                );
             window[element.dataset["template_update_callback"]](item, value);
         }
+
         if (
             templateValues[item] === "" &&
             element.dataset["template_fallback"] !== undefined
-        )
+        ) {
+            if (debug)
+                console.log(
+                    `[templateSDK] Using fallback value ${element.dataset["template_fallback"]} on template ${item}.`
+                );
             value = element.dataset["template_fallback"];
+        }
 
         if (
             element.dataset["template_insert"] === undefined ||
             element.dataset["template_insert"] === "innerText"
-        )
+        ) {
+            if (debug)
+                console.log(
+                    `[templateSDK] Replacing template ${item} with value ${value}.`
+                );
             element.innerText = value;
-        else element[element.dataset["template_insert"]] = value;
+        } else {
+            if (debug) {
+                console.log(
+                    `[templateSDK] Replacing template ${item} with value ${value}.`
+                );
+                console.log(
+                    `[templateSDK] Using custom insert method ${element.dataset["template_insert"]}.`
+                );
+            }
+
+            element[element.dataset["template_insert"]] = value;
+        }
     }
 
-    if (elements.length === 0)
+    if (elements.length === 0 && debug)
         throw new Error(
             "[templateSDK] No templates found to replace. Please make sure you have at least one element with the data-template attribute."
         );
