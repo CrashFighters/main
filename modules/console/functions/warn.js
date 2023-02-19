8f4f330dfd8285cd2eea6a1422fabf820ec71c0b
daanschenkel <daanschenkel@users.noreply.github.com>
2023-02-19 20:56:42 +0000

    Auto minify & compatibility layer

--- modules/console/functions/warn.js
+++ modules/console/functions/warn.js
@@ -1,14 +1,1 @@
-const fs = require('fs');
-const settings = require('../settings.json')
-
-module.exports = {
-    execute(text) {
-        if (!text) text = "";
-
-        let cConsole = fs.readFileSync(`.${settings.path.files.console}`);
-        cConsole = `${cConsole}${text}\n`;
-        fs.writeFileSync(`.${settings.path.files.console}`, cConsole);
-
-        console.warn(text);
-    }
-}
+const fs=require("fs"),settings=require("../settings.json");module.exports={execute(e){e||(e="");let s=fs.readFileSync(`.${settings.path.files.console}`);s=`${s}${e}\n`,fs.writeFileSync(`.${settings.path.files.console}`,s),console.warn(e)}};
