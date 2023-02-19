c8cc0ebb6b1b05f23cd7520346391b77f35877b1
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-19 14:46:41 +0100

    Made index.js beautiful

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const settings = require('./settings.json');
 let server;
 
 server = http.createServer(                         				//Create server
-	require('./main/functions/error/lastFallback').serverExecute	//Error handler
+    require('./main/functions/error/lastFallback').serverExecute	//Error handler
 );
 
 try {
 
-	//Evaluate errors
-	require('./main/functions/error/evalErrors').execute();
+    //Evaluate errors
+    require('./main/functions/error/evalErrors').execute();
 
 } catch (e) { }
 
-const port = process.env.PORT  || settings.generic.port;
+const port = process.env.PORT || settings.generic.port;
 
 server.listen(port);
 
