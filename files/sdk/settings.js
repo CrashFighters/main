46cf398648461ee7ee45e544e8200e81866e95f9
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-20 17:27:16 +0100

    Finished enable2fa

--- files/sdk/settings.js
+++ files/sdk/settings.js
@@ -1,11 +1,37 @@
 import {
     updateProfile,
-    multiFactor
+    multiFactor,
+    PhoneAuthProvider,
+    RecaptchaVerifier,
+    PhoneMultiFactorGenerator
 } from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js';
 
 import '/sdk/auth.js';
 
 const { updateUserObject, onStateChangeCallbacks, firebase: { auth } } = (await import('/sdk/auth.js'))._;
+
+let recaptchaDoneCallbacks = [];
+let recaptchaCalled = false;
+const __recaptchaVerifier = new RecaptchaVerifier('sign-in-button', {
+    size: 'invisible',
+    callback: () => {
+        for (const recaptchaDoneCallback of recaptchaDoneCallbacks)
+            recaptchaDoneCallback(__recaptchaVerifier);
+        recaptchaCalled = true;
+    }
+}, auth);
+
+function waitRecaptchaSuccess() {
+    if (recaptchaCalled) {
+        recaptchaDoneCallbacks = [];
+        __recaptchaVerifier.clear();
+        recaptchaCalled = false;
+    };
+
+    return new Promise(res => {
+        recaptchaDoneCallbacks.push(res);
+    });
+}
 
 export const setDisplayName = async (displayName) => {
     if (!window.auth.user)
@@ -38,14 +64,31 @@ export const setPicture = async (picture) => {
         callback(window.auth.user);
 };
 
-export const enable2fa = async () => {
+export const enable2fa = async (phoneNumber, displayName) => {
     if (!window.auth.user)
-        throw new Error('User is not logged in')
+        throw new Error('User is not logged in');
 
     const multiFactorUser = multiFactor(auth.currentUser);
     if (multiFactorUser.enrolledFactors.length > 0)
         throw new Error('User already has 2fa enabled');
 
-    // multiFactorUser.enroll()
-    throw new Error('not implemented'); //todo
+    const multiFactorSession = await multiFactorUser.getSession();
+    const phoneInfoOptions = {
+        phoneNumber: phoneNumber,
+        session: multiFactorSession
+    };
+
+    const recaptchaVerifier = await waitRecaptchaSuccess();
+
+    const phoneAuthProvider = new PhoneAuthProvider(auth);
+    phoneAuthProvider.verifyPhoneNumber(phoneInfoOptions, recaptchaVerifier);
+
+    return async (verificationId, verificationCode) => {
+        // when user claims they have confirmed the code, can be called multiple times
+
+        const credential = PhoneAuthProvider.credential(verificationId, verificationCode);
+        const multiFactorAssertion = PhoneMultiFactorGenerator.assertion(credential);
+        multiFactor(auth.currentUser).enroll(multiFactorAssertion, displayName);
+    };
+
 }
