f3578164b2da96260a296fc4c91a7c57f664cbf8
daanschenkel <20974756+daanschenkel@users.noreply.github.com>
2023-02-22 20:48:56 +0100

    Added single quotes

--- files/login/index.js
+++ files/login/index.js
@@ -1,57 +1,57 @@
-const signUpButton = document.getElementById("signUp");
-const signInButton = document.getElementById("signIn");
-const container = document.getElementById("container");
-const signUpButton_mobile = document.getElementById("signUp_mobile");
-const signInButton_mobile = document.getElementById("signIn_mobile");
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+const signUpButton_mobile = document.getElementById('signUp_mobile');
+const signInButton_mobile = document.getElementById('signIn_mobile');
 
-const alertScript = document.createElement("script");
-alertScript.src = "https://cdn.jsdelivr.net/npm/sweetalert2@11";
+const alertScript = document.createElement('script');
+alertScript.src = 'https://cdn.jsdelivr.net/npm/sweetalert2@11';
 document.head.appendChild(alertScript);
 
 function coolAlert(message) {
     Swal.fire({
         backdrop: true,
-        title: "Thats a crash!",
+        title: 'Thats a crash!',
         text: message,
-        type: "error",
-        confirmButtonText: "Alright",
+        type: 'error',
+        confirmButtonText: 'Alright',
         //button color
-        confirmButtonColor: "#000",
+        confirmButtonColor: '#000',
     });
 }
 
 //remove old listeners
 //todo: removeEventListener with new function doesn't do anything. Why is this here?
-signUpButton.removeEventListener("click", () => {
-    container.classList.add("right-panel-active");
+signUpButton.removeEventListener('click', () => {
+    container.classList.add('right-panel-active');
 });
-signInButton.removeEventListener("click", () => {
-    container.classList.remove("right-panel-active");
+signInButton.removeEventListener('click', () => {
+    container.classList.remove('right-panel-active');
 });
-signUpButton_mobile.removeEventListener("click", () => {
-    container.classList.add("right-panel-active");
+signUpButton_mobile.removeEventListener('click', () => {
+    container.classList.add('right-panel-active');
 });
-signInButton_mobile.removeEventListener("click", () => {
-    container.classList.remove("right-panel-active");
-});
-
-signUpButton.addEventListener("click", () => {
-    container.classList.add("right-panel-active");
+signInButton_mobile.removeEventListener('click', () => {
+    container.classList.remove('right-panel-active');
 });
 
-signInButton.addEventListener("click", () => {
-    container.classList.remove("right-panel-active");
+signUpButton.addEventListener('click', () => {
+    container.classList.add('right-panel-active');
 });
 
-signUpButton_mobile.addEventListener("click", () => {
-    container.classList.add("right-panel-active");
+signInButton.addEventListener('click', () => {
+    container.classList.remove('right-panel-active');
 });
 
-signInButton_mobile.addEventListener("click", () => {
-    container.classList.remove("right-panel-active");
+signUpButton_mobile.addEventListener('click', () => {
+    container.classList.add('right-panel-active');
 });
 
-import { onStateChange } from "/sdk/auth.js";
+signInButton_mobile.addEventListener('click', () => {
+    container.classList.remove('right-panel-active');
+});
+
+import { onStateChange } from '/sdk/auth.js';
 
 import {
     loginWithEmail,
@@ -60,24 +60,24 @@ import {
     get2faMethods,
     send2fa,
     loginWith2fa,
-} from "/sdk/login.js";
+} from '/sdk/login.js';
 
-import { setDisplayName } from "/sdk/settings.js";
+import { setDisplayName } from '/sdk/settings.js';
 
 import {
     minimalLoginRecaptchaScore,
     minimalSignupRecaptchaScore,
-} from "/common/settings.js";
+} from '/common/settings.js';
 
-import { createButton } from "/sdk/recaptcha.js";
+import { createButton } from '/sdk/recaptcha.js';
 
 const urlParams = new URLSearchParams(window.location.search);
-const signup = urlParams.get("signup") === "true";
+const signup = urlParams.get('signup') === 'true';
 if (signup)
-    document.getElementById("container").classList.add("right-panel-active");
+    document.getElementById('container').classList.add('right-panel-active');
 
 function redirect() {
-    const redirectLocation = urlParams.get("redirect");
+    const redirectLocation = urlParams.get('redirect');
 
     let doRedirect = redirectLocation !== null;
     if (
@@ -85,11 +85,11 @@ function redirect() {
         new URL(redirectLocation).origin !== window.location.origin
     )
         doRedirect = confirm(
-            `Do you want to continue to the external website "${redirectLocation}"?`
+            `Do you want to continue to the external website '${redirectLocation}'?`
         );
 
     if (doRedirect) window.location.replace(redirectLocation);
-    else window.location.replace("/");
+    else window.location.replace('/');
 }
 
 let preventRedirect = false;
@@ -98,76 +98,76 @@ onStateChange((user) => {
 });
 
 const firebaseErrorCodes = {
-    "auth/user-not-found": {
-        errorCode: "emailDoesNotExist",
-        field: "email",
+    'auth/user-not-found': {
+        errorCode: 'emailDoesNotExist',
+        field: 'email',
     },
-    "auth/missing-email": {
-        errorCode: "missingEmail",
-        field: "email",
+    'auth/missing-email': {
+        errorCode: 'missingEmail',
+        field: 'email',
     },
-    "auth/invalid-email": {
-        errorCode: "invalidEmail",
-        field: "email",
+    'auth/invalid-email': {
+        errorCode: 'invalidEmail',
+        field: 'email',
     },
-    "auth/email-already-in-use": {
-        errorCode: "emailAlreadyInUse",
-        field: "email",
+    'auth/email-already-in-use': {
+        errorCode: 'emailAlreadyInUse',
+        field: 'email',
     },
-    "auth/wrong-password": {
-        errorCode: "wrongPassword",
-        field: "password",
+    'auth/wrong-password': {
+        errorCode: 'wrongPassword',
+        field: 'password',
     },
-    "auth/weak-password": {
-        errorCode: "weakPassword",
-        field: "password",
+    'auth/weak-password': {
+        errorCode: 'weakPassword',
+        field: 'password',
     },
-    "auth/too-many-requests": {
-        errorCode: "tooManyRequests",
+    'auth/too-many-requests': {
+        errorCode: 'tooManyRequests',
     },
-    "auth/multi-factor-auth-required": {
-        errorCode: "2faRequired",
+    'auth/multi-factor-auth-required': {
+        errorCode: '2faRequired',
     },
-    "auth/invalid-verification-code": {
-        errorCode: "invalidVerificationCode",
-        field: "verificationCode",
+    'auth/invalid-verification-code': {
+        errorCode: 'invalidVerificationCode',
+        field: 'verificationCode',
     },
-    "auth/internal-error": {
-        errorCode: "firebaseAuthInternalError",
+    'auth/internal-error': {
+        errorCode: 'firebaseAuthInternalError',
     },
 };
 
 const recaptchaStateNames = {
-    ready: "recaptchaNotSolved",
-    expired: "recaptchaExpired",
-    error: "recaptchaError",
+    ready: 'recaptchaNotSolved',
+    expired: 'recaptchaExpired',
+    error: 'recaptchaError',
 };
 
 const errorCodeMessages = {
     //todo: put in en.json
-    emailDoesNotExist: "There is no account associated with this email address",
-    invalidEmail: "This isn't a valid email address",
-    missingEmail: "Please enter an email address",
-    emailAlreadyInUse: "This email address is already in use",
-    wrongPassword: "The password is incorrect",
-    weakPassword: "The password is too weak",
-    recaptchaNotSolved: "Please solve the captcha",
-    recaptchaExpired: "The captcha has expired. Please solve it again",
-    recaptchaError: "Please try the captcha again",
-    noName: "Please enter a name",
-    "2faRequired": "2FA is required for this account",
-    tooManyRequests: "Too many tries. Please try again later",
-    invalidVerificationCode: "Invalid verification code",
+    emailDoesNotExist: 'There is no account associated with this email address',
+    invalidEmail: 'This is not a valid email address',
+    missingEmail: 'Please enter an email address',
+    emailAlreadyInUse: 'This email address is already in use',
+    wrongPassword: 'The password is incorrect',
+    weakPassword: 'The password is too weak',
+    recaptchaNotSolved: 'Please solve the captcha',
+    recaptchaExpired: 'The captcha has expired. Please solve it again',
+    recaptchaError: 'Please try the captcha again',
+    noName: 'Please enter a name',
+    '2faRequired': '2FA is required for this account',
+    tooManyRequests: 'Too many tries. Please try again later',
+    invalidVerificationCode: 'Invalid verification code',
     firebaseAuthInternalError:
-        "An internal error in Firebase authentication occurred. Please try again later",
+        'An internal error in Firebase authentication occurred. Please try again later',
 };
 
 const loginFields = [
-    "email",
-    "password",
-    "recaptcha",
-    "verificationCode",
-    "2fa-recaptcha",
+    'email',
+    'password',
+    'recaptcha',
+    'verificationCode',
+    '2fa-recaptcha',
 ];
 function handleLoginError({ errorCode, field, error }) {
     const message =
@@ -175,7 +175,7 @@ function handleLoginError({ errorCode, field, error }) {
         errorCode ??
         (error?.message
             ? `Error: ${error.message}`
-            : "An unknown error occurred");
+            : 'An unknown error occurred');
 
     if (!field) return coolAlert(message);
 
@@ -188,7 +188,7 @@ function handleLoginError({ errorCode, field, error }) {
         );
 
         if (loginFieldId === field) feedbackElement.innerText = message;
-        else feedbackElement.innerText = "";
+        else feedbackElement.innerText = '';
     }
 }
 window.removeLoginErrorFeedback = () => {
@@ -196,25 +196,25 @@ window.removeLoginErrorFeedback = () => {
         const feedbackElement = document.getElementById(
             `login-${loginFieldId}-feedback`
         );
-        feedbackElement.innerText = "";
+        feedbackElement.innerText = '';
     }
 };
 
 function choose2faMethod(_2faMethods) {
-    const chooseText = document.getElementById("choose2faMethodText");
-    const choose = document.getElementById("choose2faMethod");
+    const chooseText = document.getElementById('choose2faMethodText');
+    const choose = document.getElementById('choose2faMethod');
 
     return new Promise((res) => {
         for (const index in _2faMethods) {
             const { displayName, phoneNumber } = _2faMethods[index];
 
-            const button = document.createElement("button");
+            const button = document.createElement('button');
             button.innerText = displayName
                 ? `${displayName} (${phoneNumber})`
                 : phoneNumber;
-            button.addEventListener("click", () => {
-                chooseText.style.display = "none";
-                choose.style.display = "none";
+            button.addEventListener('click', () => {
+                chooseText.style.display = 'none';
+                choose.style.display = 'none';
                 res(index);
             });
 
@@ -229,7 +229,7 @@ function choose2faMethod(_2faMethods) {
 function wait2faRecaptchaSuccess(recaptchaObject) {
     return new Promise((res) => {
         recaptchaObject.onStateChange(() => {
-            if (["success", "successBefore"].includes(recaptchaObject.state))
+            if (['success', 'successBefore'].includes(recaptchaObject.state))
                 res();
         });
     });
@@ -240,28 +240,28 @@ async function enable2fa(error) {
     _2faError = error;
 
     const _2faRecaptchaContainer = document.getElementById(
-        "_2faRecaptchaContainer"
+        '_2faRecaptchaContainer'
     );
 
-    const emailElement = document.getElementById("loginEmail");
-    const passwordElement = document.getElementById("loginPassword");
+    const emailElement = document.getElementById('loginEmail');
+    const passwordElement = document.getElementById('loginPassword');
 
-    const nativeButton = document.getElementById("loginButton-1");
+    const nativeButton = document.getElementById('loginButton-1');
     const signupRecaptchaButton = document.getElementById(
-        "signupRecaptchaButton"
+        'signupRecaptchaButton'
     );
-    const forgotPassword = document.getElementById("forgotPassword");
+    const forgotPassword = document.getElementById('forgotPassword');
     const loginRecaptchaButton = document.getElementById(
-        "loginRecaptchaButton"
+        'loginRecaptchaButton'
     );
 
     const verificationCodeInput = document.getElementById(
-        "verificationCodeInput"
+        'verificationCodeInput'
     );
-    const verify2faButton = document.getElementById("verify2faButton");
+    const verify2faButton = document.getElementById('verify2faButton');
 
     const verificationCodeStatus = document.getElementById(
-        "2faVerificationCodeStatus"
+        '2faVerificationCodeStatus'
     );
 
     _2faRecaptchaContainer.style.display = null;
@@ -269,10 +269,10 @@ async function enable2fa(error) {
     emailElement.disabled = true;
     passwordElement.disabled = true;
 
-    nativeButton.style.display = "none";
-    signupRecaptchaButton.style.display = "none";
-    forgotPassword.style.display = "none";
-    loginRecaptchaButton.style.display = "none";
+    nativeButton.style.display = 'none';
+    signupRecaptchaButton.style.display = 'none';
+    forgotPassword.style.display = 'none';
+    loginRecaptchaButton.style.display = 'none';
 
     const recaptchaObject = await prepare2fa();
 
@@ -282,40 +282,40 @@ async function enable2fa(error) {
     if (_2faMethods.length === 1) selectedIndex = 0;
     else selectedIndex = await choose2faMethod(_2faMethods);
 
-    if (!["success", "successBefore"].includes(recaptchaObject.state))
+    if (!['success', 'successBefore'].includes(recaptchaObject.state))
         await wait2faRecaptchaSuccess(recaptchaObject);
 
-    _2faRecaptchaContainer.style.display = "none";
+    _2faRecaptchaContainer.style.display = 'none';
 
     verificationCodeInput.style.display = null;
 
     verify2faButton.style.opacity = 0;
     verify2faButton.style.display = null;
 
-    verificationCodeStatus.innerText = "Sending a verification code";
+    verificationCodeStatus.innerText = 'Sending a verification code';
     verificationCodeStatus.style.display = null;
 
     const { phoneNumber, displayName } = await send2fa(selectedIndex);
 
     verificationCodeStatus.innerText =
-        "A verification code has been sent to {location}".replace(
-            "{location}",
+        'A verification code has been sent to {location}'.replace(
+            '{location}',
             displayName ? `${displayName} (${phoneNumber})` : phoneNumber
         );
 
     verify2faButton.addEventListener(
-        "click",
+        'click',
         () => (verify2faButton.disabled = true)
     );
     verify2faButton.style.opacity = null;
 }
 
 window.verify2fa = async () => {
-    if (!_2faError) throw new Error("No 2FA error found");
+    if (!_2faError) throw new Error('No 2FA error found');
 
-    const verify2faButton = document.getElementById("verify2faButton");
+    const verify2faButton = document.getElementById('verify2faButton');
     const verificationCode = document.getElementById(
-        "verificationCodeInput"
+        'verificationCodeInput'
     ).value;
     try {
         await loginWith2fa(verificationCode);
@@ -333,20 +333,20 @@ window.verify2fa = async () => {
 
 let loginRecaptcha;
 window.doLogin = async (recaptchaScore) => {
-    const email = document.getElementById("loginEmail").value;
-    const password = document.getElementById("loginPassword").value;
-    const nativeButton = document.getElementById("loginButton-1");
+    const email = document.getElementById('loginEmail').value;
+    const password = document.getElementById('loginPassword').value;
+    const nativeButton = document.getElementById('loginButton-1');
 
     // create login captcha if user is likely a bot
     if (recaptchaScore < minimalLoginRecaptchaScore && !loginRecaptcha)
         loginRecaptcha = await createButton(
-            document.getElementById("loginRecaptchaButton")
+            document.getElementById('loginRecaptchaButton')
         );
 
-    if (loginRecaptcha && loginRecaptcha.state !== "success")
+    if (loginRecaptcha && loginRecaptcha.state !== 'success')
         return handleLoginError({
             errorCode: recaptchaStateNames[loginRecaptcha.state],
-            field: "recaptcha",
+            field: 'recaptcha',
         });
 
     nativeButton.disabled = true;
@@ -355,7 +355,7 @@ window.doLogin = async (recaptchaScore) => {
         window.removeLoginErrorFeedback();
         await loginWithEmail(email, password);
     } catch (e) {
-        if (e.code === "auth/multi-factor-auth-required") return enable2fa(e);
+        if (e.code === 'auth/multi-factor-auth-required') return enable2fa(e);
 
         const firebaseErrorCode = firebaseErrorCodes[e.code];
         return handleLoginError({
@@ -371,14 +371,14 @@ window.doLogin = async (recaptchaScore) => {
     redirect();
 };
 
-const signupFields = ["name", "email", "password", "recaptcha"];
+const signupFields = ['name', 'email', 'password', 'recaptcha'];
 function handleSignupError({ errorCode, field, error }) {
     const message =
         errorCodeMessages[errorCode] ??
         errorCode ??
         (error?.message
             ? `Error: ${error.message}`
-            : "An unknown error occurred");
+            : 'An unknown error occurred');
 
     if (!field) return coolAlert(message);
 
@@ -391,7 +391,7 @@ function handleSignupError({ errorCode, field, error }) {
         );
 
         if (signupFieldId === field) feedbackElement.innerText = message;
-        else feedbackElement.innerText = "";
+        else feedbackElement.innerText = '';
     }
 }
 window.removeSignupErrorFeedback = () => {
@@ -399,30 +399,30 @@ window.removeSignupErrorFeedback = () => {
         const feedbackElement = document.getElementById(
             `signup-${signupFieldId}-feedback`
         );
-        feedbackElement.innerText = "";
+        feedbackElement.innerText = '';
     }
 };
 
 let signupRecaptcha;
 window.doSignup = async (recaptchaScore) => {
-    const name = document.getElementById("signupName").value;
-    const email = document.getElementById("signupEmail").value;
-    const password = document.getElementById("signupPassword").value;
-    const nativeButton = document.getElementById("signupButton-1");
+    const name = document.getElementById('signupName').value;
+    const email = document.getElementById('signupEmail').value;
+    const password = document.getElementById('signupPassword').value;
+    const nativeButton = document.getElementById('signupButton-1');
 
     if (name.length === 0)
-        return handleSignupError({ errorCode: "noName", field: "name" });
+        return handleSignupError({ errorCode: 'noName', field: 'name' });
 
     // create login captcha if user is likely a bot
     if (recaptchaScore < minimalSignupRecaptchaScore && !signupRecaptcha)
         signupRecaptcha = await createButton(
-            document.getElementById("signupRecaptchaButton")
+            document.getElementById('signupRecaptchaButton')
         );
 
-    if (signupRecaptcha && signupRecaptcha.state !== "success")
+    if (signupRecaptcha && signupRecaptcha.state !== 'success')
         return handleSignupError({
             errorCode: recaptchaStateNames[loginRecaptcha.state],
-            field: "recaptcha",
+            field: 'recaptcha',
         });
 
     nativeButton.disabled = true;
