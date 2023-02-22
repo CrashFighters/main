abf4eda5de0e42c2848e1766115702e8e5c42b35
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-22 23:03:03 +0100

    Created simple language sdk

---
+++ files/sdk/language.js
@@ -0,0 +1,13 @@
+import { deepQuerySelectorAll } from '/common/deepQuerySelectorAll.js';
+
+let messages = await fetch('/api/messages');
+messages = await messages.json();
+
+function getMessage(message) {
+    return messages.pages[window.location.pathname]?.[message] || messages.general[message] || message;
+};
+
+const elements = deepQuerySelectorAll('[data-lang_text]');
+
+for (const element of elements)
+    element.innerText = getMessage(element.dataset.lang_text);
