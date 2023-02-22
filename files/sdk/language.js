72d1938db0b23b6f8d7905a21817092e68a3bde4
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-22 23:16:02 +0100

    Added new check for capitalization

--- files/sdk/language.js
+++ files/sdk/language.js
@@ -4,8 +4,19 @@ let messages = await fetch('/api/messages');
 messages = await messages.json();
 
 export function getMessage(message) {
-    return messages.pages[window.location.pathname]?.[message] || messages.general[message] || message;
+    return findMessageInMessages(message) ||
+        (findMessageInMessages(flipFirstLetterCase(message)) ?
+            flipFirstLetterCase(findMessageInMessages(flipFirstLetterCase(message))) :
+            message);
 };
+
+function findMessageInMessages(message) {
+    return messages.pages[window.location.pathname]?.[message] || messages.general[message];
+}
+
+function flipFirstLetterCase(string) {
+    return ((string[0].toUpperCase() === string[0]) ? string[0].toLowerCase() : string[0].toUpperCase()) + string.slice(1);
+}
 
 const elements = deepQuerySelectorAll('[data-lang_text]');
 
