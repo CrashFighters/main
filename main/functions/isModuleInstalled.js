140dd3d35eb411460d2e13c263f21f91cf5ea6a8
daanschenkel <20974756+daanschenkel@users.noreply.github.com>
2023-02-22 19:44:26 +0100

    Release

--- main/functions/isModuleInstalled.js
+++ main/functions/isModuleInstalled.js
@@ -1,1 +1,8 @@
-const fs=require("fs"),settings=require("../../settings.json");module.exports={execute:e=>fs.existsSync(`${settings.generic.path.files.modules}${e}/`)};
+const fs = require('fs');
+const settings = require('../../settings.json');
+
+module.exports = {
+    execute(name) {
+        return fs.existsSync(`${settings.generic.path.files.modules}${name}/`);
+    }
+}
--- modules/database/functions/database.js
+++ modules/database/functions/database.js
@@ -1,1 +1,53 @@
-let admin,serviceAccount,databaseURL,db,ref,err,data=null,hasInit=!1;function init(){console.log("Initializing database"),admin=require("firebase-admin"),serviceAccount=require("../../../credentials/firebase.json").serviceAccount,databaseURL=require("../../../credentials/firebase.json").databaseURL,admin.initializeApp({credential:admin.credential.cert(serviceAccount),databaseURL:databaseURL}),db=admin.database(),ref=db.ref(),ref.on("value",(e=>{data=e.val()}),(e=>{err=e})),hasInit=!0}module.exports={set(e){hasInit||init(),ref.set(e)},get(){if(hasInit||init(),err)throw err;return data||{}}};
+let admin;
+let serviceAccount
+let databaseURL;
+
+let db;
+let ref;
+
+let data = null;
+let err;
+
+let hasInit = false;
+function init() {
+    console.log('Initializing database');
+
+    admin = require('firebase-admin');
+    serviceAccount = require('../../../credentials/firebase.json').serviceAccount;
+    databaseURL = require('../../../credentials/firebase.json').databaseURL;
+
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL
+    });
+
+    db = admin.database();
+    ref = db.ref();
+
+    ref.on('value', snapshot => {
+        data = snapshot.val();
+    }, error => {
+        err = error;
+    });
+
+    hasInit = true;
+};
+
+module.exports = {
+    set(val) {
+        if (!hasInit)
+            init();
+
+        ref.set(val)
+    },
+    get() {
+        if (!hasInit)
+            init()
+
+        if (err)
+            throw err;
+
+        if (!data) return {};
+        return data;
+    }
+}
