9237aba2a06d94366045e69c7ad079615c676e13
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-22 17:49:37 +0100

    Implemented post requests in main

--- main/server/main.js
+++ main/server/main.js
@@ -1,19 +1,26 @@
 const fs = require('fs');
 const path = require('path');
+
 const middlewares = fs.existsSync(path.resolve(__dirname, './middleware/')) ?
     fs.readdirSync(path.resolve(__dirname, './middleware/')).map(a => require(`./middleware/${a}`)) :
     null;
 
 const settings = require('../../settings.json');
 const parseErrorOnline = require('../functions/error/parseErrorOnline').execute;
+const parsePostBody = require('../functions/parse/postBody');
 
 module.exports = {
     async execute(request, response) {
         const parseError = (error, customText) => parseErrorOnline(error, response, customText);
 
         try {
+            let body;
+            if (request.method === 'POST')
+                body = parsePostBody(await waitPost(request));
 
-            let middlewareData = {};
+            let middlewareData = {
+                body
+            };
             if (middlewares)
                 for (const middleware of middlewares) {
                     const newMiddlewareData = await middleware(request, response);
@@ -29,4 +36,22 @@ module.exports = {
             parseError(err);
         }
     }
+}
+
+function waitPost(request) {
+    return new Promise(res => {
+
+        let body = '';
+        request.on('data', data => {
+            body += data;
+
+            if (body.length > 1e6)
+                request.connection.destroy();
+        });
+
+        request.on('end', () => {
+            res(body);
+        });
+
+    });
 }
