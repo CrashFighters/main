303f99bb7bea86d1de05bd6fa783dd1e7bcde459
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-23 13:06:04 +0100

    Started working on api

--- main/server/dbApi.js
+++ main/server/dbApi.js
@@ -27,14 +27,55 @@ module.exports = {
                 path,
                 params,
                 method: request.method,
-                requireParams: (params) => {
+                require: (params, { community, post }, types) => {
                     for (const { name, type } of params) {
-                        if (!params.name) {
+                        if (types.includes('correctType') && !params[name]) {
                             statusCode(response, 400, { text: `Missing parameter; ${name}`, short: 'missingParameter' });
                             return false;
                         }
 
-                        //todo: check if param has correct type. Example types: communityId, userId, postId, string
+                        if (type === 'communityId') {
+                            const community = db.communities[params[name]];
+
+                            if (types.includes('correctType') && !community) {
+                                statusCode(response, 404, { text: `No community found with id ${params[name]}`, short: 'communityNotFound' });
+                                return false;
+                            }
+                        } else if (type === 'postId') {
+                            if (!community)
+                                throw new Error("Can't get post without community");
+
+                            const post = db.communities[community].posts[params[name]];
+
+                            if (types.includes('correctType') && !post) {
+                                statusCode(response, 404, { text: `No post found with id ${params[name]} in community ${community}`, short: 'postNotFound' });
+                                return false;
+                            }
+                        } else if (type === 'voteIndex') {
+                            if (!community)
+                                throw new Error("Can't get vote without community");
+
+                            if (!post)
+                                throw new Error("Can't get vote without post");
+
+                            const vote = db.communities[community].posts[post].votes[params[name]];
+
+                            if (types.includes('correctType') && !vote) {
+                                statusCode(response, 404, { text: `No vote found with index ${params[name]} in post ${post} in community ${community}`, short: 'voteNotFound' });
+                                return false;
+                            }
+                        } else if (type === 'boolean') {
+                            if (types.includes('correctType') && typeof params[name] !== 'boolean') {
+                                statusCode(response, 400, { text: `Parameter ${name} is not a boolean`, short: 'notBoolean' });
+                                return false;
+                            }
+                        } else if (type === 'string') {
+                            if (types.includes('correctType') && typeof params[name] !== 'string') {
+                                statusCode(response, 400, { text: `Parameter ${name} is not a string`, short: 'notString' });
+                                return false;
+                            }
+                        } else
+                            throw new Error(`Unknown type ${type}`)
                     }
 
                     return true;
@@ -49,6 +90,6 @@ module.exports = {
     }
 }
 
-function doApiCall({ db, set, path, params, method }) {
+function doApiCall({ db, set, path, params, method, require }) {
     throw new Error('not implemented')
 }
