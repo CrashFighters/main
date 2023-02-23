a23670807370b6322e1395a14c8ca009825a8870
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-23 23:43:46 +0100

    Updated database sdk

--- files/sdk/database.js
+++ files/sdk/database.js
@@ -5,13 +5,11 @@ import {
 const { auth } = (await import('/sdk/auth.js'))._.firebase;
 
 export const _ = {
-    async getRequestOptions() {
+    async getAuthHeaders() {
         const userIdToken = await getIdToken(auth.currentUser);
 
         return {
-            headers: {
-                auth_token: userIdToken
-            }
+            auth_token: userIdToken
         }
 
     }
