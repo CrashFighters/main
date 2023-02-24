0eaab1a0c03976a01e5b55ad282a2cfc14145125
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-24 17:39:45 +0100

    Made error message clearer

--- main/server/dbApi.js
+++ main/server/dbApi.js
@@ -318,7 +318,7 @@ function doApiCall({ db, set, path, params, method, require, end, statusCode, us
             return;
         }
     } else {
-        statusCode(404, { text: 'Not found', short: 'notFound' });
+        statusCode(404, { text: 'Endpoint not found', short: 'endpointNotFound' });
         return;
     }
 }
