3d702501e478656bb2e3f9b7cf2c6146f1c08ed8
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-25 16:55:15 +0100

    Implemented getRoles

---
+++ modules/authentication/functions/getRoles.js
@@ -0,0 +1,12 @@
+module.exports = (user) => {
+    const userRoles = [];
+
+    userRoles.push('roles:default');
+    if (user) userRoles.push('roles:authenticated');
+
+    if (user?.customClaims?.roles)
+        for (const role of user.customClaims.roles)
+            userRoles.push(`customRoles:${role}`);
+
+    return userRoles;
+}
