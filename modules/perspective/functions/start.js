2dbe627c4d795fb3ff5d73dce6b4dbb7cae3148d
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-25 23:12:25 +0100

    Put perspective into function

--- modules/perspective/functions/start.js
+++ modules/perspective/functions/start.js
@@ -7,21 +7,26 @@ const path = require('path');
 const wait = ms => new Promise(res => setTimeout(res, ms));
 
 module.exports = async () => {
-    while (true) {
-        await wait(1500); // Perspective API has a rate limit of 60 requests per minute, so with 1.5s we're safe
+    setTimeout(execute, 0);
+}
 
-        const { community, post } = require('../data/queue.json')[0];
+async function execute() {
+    await wait(1500); // Perspective API has a rate limit of 60 requests per minute, so with 1.5s we're safe
 
-        const db = get();
-        const dbPost = db.communities?.[community]?.posts?.[post];
+    if (require('../data/queue.json').length === 0) return setTimeout(execute, 0);
+    const { community, post } = require('../data/queue.json')[0];
 
-        if (dbPost) {
-            const result = await perspective(dbPost.message);
-            dbPost.perspective = result;
-            set(db);
-        }
+    const db = get();
+    const dbPost = db.communities?.[community]?.posts?.[post];
 
-        const newQueue = require('../data/queue.json').slice(1);
-        fs.writeFileSync(path.resolve(__dirname, '../data/queue.json'), JSON.stringify(newQueue));
+    if (dbPost) {
+        const result = await perspective(dbPost.message);
+        dbPost.perspective = result;
+        set(db);
     }
+
+    const newQueue = require('../data/queue.json').slice(1);
+    fs.writeFileSync(path.resolve(__dirname, '../data/queue.json'), JSON.stringify(newQueue));
+
+    setTimeout(execute, 0);
 }
