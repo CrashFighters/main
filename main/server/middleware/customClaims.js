2e883e906171f75de7b4dc0f46f61c930b57e594
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-25 19:19:49 +0100

    Created customClaims middleware

---
+++ main/server/middleware/customClaims.js
@@ -0,0 +1,18 @@
+const firebase = require('../../../modules/authentication/functions/authentication.js');
+
+module.exports = {
+    info: {
+        requires: ['authentication']
+    },
+    async execute({ parseError, middlewareData: { authentication } }) {
+        try {
+            if (!authentication) return { customClaims: {} };
+
+            const firebaseUser = await firebase.auth().getUser(authentication.sub);
+            const customClaims = firebaseUser.customClaims;
+            return customClaims;
+        } catch (e) {
+            parseError(e);
+        }
+    }
+}
