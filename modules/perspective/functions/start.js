a92862a25b87d2b36b9c125d5c7629fbe7c13c79
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-25 23:28:00 +0100

    Fixed bug in perspective start

--- modules/perspective/functions/start.js
+++ modules/perspective/functions/start.js
@@ -4,29 +4,29 @@ const perspective = require('./perspective.js');
 const fs = require('fs');
 const path = require('path');
 
-const wait = ms => new Promise(res => setTimeout(res, ms));
+// const wait = ms => new Promise(res => setTimeout(res, ms));
 
 module.exports = async () => {
-    setTimeout(execute, 0);
+    setTimeout(execute, 1500);
 }
 
 async function execute() {
-    await wait(1500); // Perspective API has a rate limit of 60 requests per minute, so with 1.5s we're safe
-
     if (require('../data/queue.json').length === 0) return setTimeout(execute, 0);
-    const { community, post } = require('../data/queue.json')[0];
 
-    const db = get();
+    const { community, post } = require('../data/queue.json')[0];
+    console.log('Processing', community, post)
+
+    const db = await get();
     const dbPost = db.communities?.[community]?.posts?.[post];
 
     if (dbPost) {
         const result = await perspective(dbPost.message);
         dbPost.perspective = result;
-        set(db);
+        await set(db);
     }
 
     const newQueue = require('../data/queue.json').slice(1);
     fs.writeFileSync(path.resolve(__dirname, '../data/queue.json'), JSON.stringify(newQueue));
 
-    setTimeout(execute, 0);
+    setTimeout(execute, 1500); // Perspective API has a rate limit of 60 requests per minute, so with 1.5s we're safe
 }
