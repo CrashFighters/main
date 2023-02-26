d0e99400f4cefa44dc226b98c2744c03242bb60c
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-26 16:53:24 +0100

    Implemented privateFiles

--- main/server/normal.js
+++ main/server/normal.js
@@ -2,23 +2,28 @@ const fs = require('fs');
 const mime = require('mime-types');
 
 const statusCode = require('../functions/error/statusCode.js').execute;
+const getPermission = require('../../modules/authentication/functions/getPermission.js');
 
 module.exports = {
-    // execute(request, response, {middlewareData, extraData}) {
-    execute(request, response) {
+    execute(request, response, { middlewareData }) {
 
-        const { publicPath, privatePath } = require('../functions/urlToPath.js').execute(request.url);
-        const path = publicPath;
-        //todo
+        const { publicPath, privatePath, localPath } = require('../functions/urlToPath.js').execute(request.url);
+        const permissionParts = localPath.split('/').slice(1);
 
-        if (fs.existsSync(path))
-            fs.readFile(path, async (err, data) => {
-                if (err) throw err;
-
-                response.writeHead(200, { 'Content-Type': mime.lookup(path) });
-                return response.end(data);
-            });
+        if (fs.existsSync(privatePath) && getPermission(['privateFiles', ...permissionParts], middlewareData?.authentication, middlewareData?.customClaims) === 'always')
+            respond(privatePath, response);
+        if (fs.existsSync(publicPath))
+            respond(publicPath, response);
         else
             statusCode(response, 404);
     }
 };
+
+function respond(path, response) {
+    fs.readFile(path, (err, data) => {
+        if (err) throw err;
+
+        response.writeHead(200, { 'Content-Type': mime.lookup(path) });
+        return response.end(data);
+    });
+}
