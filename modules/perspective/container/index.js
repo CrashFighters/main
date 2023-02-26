abc08f6bd1ccfd2c456a8ed515ba2e4eb2d5d398
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-26 12:54:12 +0100

    Removed logs from perspective container

--- modules/perspective/container/index.js
+++ modules/perspective/container/index.js
@@ -1,4 +1,5 @@
 console.log('Running perspective container');
+const debugLog = () => { };
 
 const { get, set } = require('../../database/functions/database.js');
 const perspective = require('./functions/perspective.js');
@@ -22,29 +23,29 @@ async function execute() {
     const queue = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/queue.json')).toString());
     if (queue.length === 0) return;
 
-    console.log()
-    console.log('\x1b[31m\x1b[1m', 'Queue:', queue.length, '\x1b[0m') // red, bright, reset
+    debugLog()
+    debugLog('\x1b[31m\x1b[1m', 'Queue:', queue.length, '\x1b[0m') // red, bright, reset
 
     const { community, post } = queue[0];
 
-    console.log()
-    console.log(community, post)
-    console.log('Getting post from database...')
+    debugLog()
+    debugLog(community, post)
+    debugLog('Getting post from database...')
 
     const db = await get();
     const dbPost = db.communities?.[community]?.posts?.[post];
 
     if (dbPost) {
-        console.log('Running perspective...')
+        debugLog('Running perspective...')
         const result = await perspective(dbPost.message);
 
-        console.log('Saving to database...')
+        debugLog('Saving to database...')
         dbPost.perspective = result;
         await set(db);
     } else
-        console.log('Post not found in database. Skipping...')
+        debugLog('Post not found in database. Skipping...')
 
-    console.log('Updating queue...')
+    debugLog('Updating queue...')
     const newQueue = queue.slice(1);
     fs.writeFileSync(path.resolve(__dirname, '../data/queue.json'), JSON.stringify(newQueue));
 }
