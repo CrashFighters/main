05709921c40d40d6d8d203c49860523f87af52fb
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-26 17:05:59 +0100

    Created setCookie

--- publicFiles/common/cookie.js
+++ publicFiles/common/cookie.js
@@ -8,6 +8,10 @@ export function getCookie(name) {
     return getCookies()[name] ?? null;
 }
 
+export function setCookie(name, value) {
+    document.cookie = `${name}=${value}`;
+}
+
 export function deleteCookie(name) {
     document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
