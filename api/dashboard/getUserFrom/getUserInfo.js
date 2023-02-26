0ee0520fe5438572702a699d38f5f16615a1d701
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-26 23:05:12 +0100

    Implemented getUserInfo

---
+++ api/dashboard/getUserFrom/getUserInfo.js
@@ -0,0 +1,73 @@
+const firebase = require('../../../modules/authentication/functions/authentication.js');
+
+module.exports = {
+    async execute({ params, statusCode, parseError, end, middlewareData: { getPermission } }) {
+        try {
+            if (!params.user) return statusCode(400, 'nosUserProved', 'No user provided');
+
+            const auth = firebase.auth();
+            let user;
+            try {
+                user = await auth.getUser(params.email);
+            } catch {
+                return statusCode(404, 'noUserFound', 'No user found');
+            }
+
+            const email = user.email ?? user.providerData[0]?.email ?? null;
+
+            let loginMethod = 'unknown';
+            if (user.providerData[0].providerId === 'password')
+                loginMethod = 'email';
+            else if (user.providerData[0].providerId === 'google.com')
+                loginMethod = 'google';
+            else if (user.providerData[0].providerId === 'github.com')
+                loginMethod = 'github';
+            else if (user.providerData[0].providerId === 'anonymous')
+                loginMethod = 'anonymous';
+
+            const multiFactorUser = user.multiFactor;
+
+            const userInfo = {
+                id: user,
+                loginMethod,
+                picture: user.photoURL,
+                displayName: user.displayName,
+                email,
+                emailVerified: user.email ? user.emailVerified : true,
+                phoneNumber: user.phoneNumber,
+                creationTime: user.metadata.creationTime,
+                lastSignInTime: user.metadata.lastSignInTime,
+                '2fa': !multiFactorUser?.enrolledFactors ? [] : multiFactorUser?.enrolledFactors.map(factor => ({
+                    creationTime: factor.enrollmentTime,
+                    type: factor.factorId,
+                    displayName: factor.displayName,
+                    phoneNumber: factor.phoneNumber,
+                    id: factor.uid
+                }))
+            };
+
+            const accessibleUserInfo = {};
+
+            for (const [key, value] of Object.entries(userInfo)) {
+                const permissionParts = ['dashboard', 'get', 'userInfo', key];
+                const permission = getPermission(permissionParts);
+
+                let hasPermission;
+                if (permission === 'always')
+                    hasPermission = true;
+                else if (hasPermission === 'never')
+                    hasPermission = false;
+                else
+                    throw new Error(`Don't know how to handle permission ${permission} (${permissionParts.join('.')})`)
+
+                if (hasPermission)
+                    accessibleUserInfo[key] = value;
+            }
+
+            end(JSON.stringify(accessibleUserInfo))
+
+        } catch (e) {
+            parseError(e)
+        }
+    }
+}
