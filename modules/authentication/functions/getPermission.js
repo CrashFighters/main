ed444d6956ab3471cb00adeaff5b0fbbca38289f
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-26 16:49:27 +0100

    Implemented _other in getPermission

--- modules/authentication/functions/getPermission.js
+++ modules/authentication/functions/getPermission.js
@@ -1,17 +1,44 @@
 const getPermissions = require('./getPermissions.js');
 
 module.exports = (_permissionParts, user, customClaims) => {
-    let currentPermission = getPermissions(user, customClaims);
+    const fullPermissions = getPermissions(user, customClaims);
+    let currentPermission = fullPermissions;
 
     let permissionParts = _permissionParts;
     if (typeof permissionParts === 'string')
         permissionParts = permissionParts.split('.');
 
-    for (const permissionPart of permissionParts)
-        if (typeof currentPermission !== 'object')
-            return currentPermission;
-        else
-            currentPermission = currentPermission[permissionPart];
+    for (const permissionPartIndex in permissionParts) {
+        const permissionPart = permissionParts[permissionPartIndex];
 
-    return currentPermission;
+        if (typeof currentPermission !== 'object') {
+            return currentPermission ?? getPermissionFromUndefined(fullPermissions, permissionParts.slice(0, permissionPartIndex));
+        } else
+            currentPermission = currentPermission[permissionPart];
+    }
+
+    return currentPermission ?? getPermissionFromUndefined(fullPermissions, permissionParts);
+}
+
+function getPermissionFromUndefined(permissions, permissionParts) {
+    for (let ii = permissionParts.length; ii >= 0; ii--) {
+        const currentPermission = getObjectValueFromPropertyArray(permissions, permissionParts.slice(0, ii));
+
+        if (currentPermission?._other) return currentPermission._other;
+    }
+
+    return 'never';
+}
+
+function getObjectValueFromPropertyArray(object, propertyArray) {
+    let currentObject = object;
+
+    for (const property of propertyArray) {
+        if (typeof currentObject !== 'object')
+            return currentObject;
+        else
+            currentObject = currentObject[property];
+    }
+
+    return currentObject;
 }
