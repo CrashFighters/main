133cea4b30c4944a06d858008bbe5ebf3c11aedc
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-26 12:33:50 +0100

    Removed debug log fro addPostToQueue

--- modules/perspective/functions/addPostToQueue.js
+++ modules/perspective/functions/addPostToQueue.js
@@ -5,8 +5,6 @@ if (!fs.existsSync(path.resolve(__dirname, '../data/queue.json')))
     fs.writeFileSync(path.resolve(__dirname, '../data/queue.json'), JSON.stringify([]));
 
 module.exports = ({ community, post }) => {
-    console.log('added to queue', community, post)
-
     const queue = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/queue.json')).toString());
 
     queue.push({ community, post });
