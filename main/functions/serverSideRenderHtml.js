ebc90bc37ee48019dad3d941c303d2f756b21aee
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-27 18:46:51 +0100

    Created server side rendering for Roboto font

--- main/functions/serverSideRenderHtml.js
+++ main/functions/serverSideRenderHtml.js
@@ -4,5 +4,12 @@ module.exports = (html, isPrivate) => {
     if (isPrivate)
         newHtml += '<script>window.privateFile = true;</script>';
 
+    newHtml = newHtml.replaceAll('<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />', `
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preload" href="styles.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+        <noscript><link rel="stylesheet" href="styles.css"></noscript>
+    `)
+
     return newHtml;
 }
