a56e5648fd74e446d0eca91821581e9a37d64880
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-27 20:37:53 +0100

    Added Cache-Control to extraHeaders

---
+++ main/functions/extraHeaders.js
@@ -0,0 +1,12 @@
+const { cache: { js: { cacheMinutes: jsCache, staleUseMinutes: jsStaleCache, errorUseMinutes: jsErrorCache } } } = require('../../settings.json');
+
+module.exports = (request, isPrivate) => {
+    const headers = {};
+
+    if (isPrivate)
+        headers['Cache-Control'] = 'private, max-age=0, no-cache, no-store, must-revalidate';
+    else
+        headers['Cache-Control'] = `public, max-age=${jsCache * 60}, stale-while-revalidate=${jsStaleCache * 60}, stale-if-error=${jsErrorCache * 60}`
+
+    return headers;
+}
