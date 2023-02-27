f2384b8a9e089dcadf93205daccbccde5d677f11
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-27 20:00:57 +0100

    Added requirements to extraHtmlHeaders

--- main/functions/extraHtmlHeaders.js
+++ main/functions/extraHtmlHeaders.js
@@ -7,22 +7,46 @@ const preloadScripts = [
     ...fs.readdirSync(path.resolve(__dirname, '../../publicFiles/common/')).map(f => `/sdk/${f}`)
 ];
 
+const requirements = {
+    '/sdk/auth.js': ['/js/appCheck.js', '/common/apiKeys.js', '/common/cookie.js'],
+    '/sdk/googleLoginButtons.js': ['/common/cookie.js', '/common/apiKeys.js', '/common/doesDocumentIncludeScript.js', '/common/isMobile.js', '/sdk/auth.js'],
+    '/sdk/language.js': ['/common/deepQuerySelectorAll.js'],
+    '/sdk/oneTap.js': ['/sdk/auth.js', '/common/apiKeys.js', '/common/doesDocumentIncludeScript.js'],
+    '/sdk/recaptcha.js': ['/common/apiKeys.js', '/common/doesDocumentIncludeScript.js'],
+    '/sdk/templates.j': ['/sdk/auth.js', '/common/deepQuerySelectorAll.js'],
+    '/sk/zeroTap.js': ['/sdk/auth.js', '/common/apiKeys.js', '/common/doesDocumentIncludeScript.js']
+};
+
 // module.exports = (html, request, isPrivate) => {
 module.exports = (html) => {
     const headers = {};
 
-    const links = [];
 
-    for (const preloadScript of preloadScripts) {
+    const loadedScripts = [];
+    for (const preloadScript of preloadScripts)
         if (html.includes(`<script type="module" src="${preloadScript}"></script>`))
-            links.push(`<${preloadScript}>; rel=preload; as=script`);
+            loadedScripts.push(preloadScript);
+
+    let changed = true;
+    while (changed) {
+        changed = false;
+        for (const preloadScript of preloadScripts)
+            if (requirements[preloadScript])
+                for (const requirement of requirements[preloadScript])
+                    if (!loadedScripts.includes(requirement)) {
+                        loadedScripts.push(requirement);
+                        changed = true;
+                    }
     };
+
+    console.log(loadedScripts)
+
+    const links = [];
+    for (const loadedScript of loadedScripts)
+        links.push(`<${loadedScript}>; rel=preload; as=script`);
 
     if (links.length > 0)
         headers['Link'] = links.join(', ');
-
-    console.log(headers)
-    process.exit()
 
     return headers;
 }
