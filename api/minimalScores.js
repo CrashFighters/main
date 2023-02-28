a4f7d333e2adff5fcbccf08c29119c318e18fd01
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-02-28 17:16:21 +0100

    Implemented cache for api

--- api/minimalScores.js
+++ api/minimalScores.js
@@ -1,6 +1,14 @@
 const { minimalScores } = require('../settings.json');
 
 module.exports = {
+    info: {
+        cache: {
+            enabled: true,
+            minutes: 60,
+            staleUseMinutes: 180,
+            errorUseMinutes: 90
+        }
+    },
     execute({ end }) {
         end(JSON.stringify(minimalScores));
     }
