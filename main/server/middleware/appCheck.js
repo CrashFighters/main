93250379429d665e66512755a09b4cf5d6681fb3
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-03-01 23:31:45 +0100

    Implemented appCheck middleware

---
+++ main/server/middleware/appCheck.js
@@ -0,0 +1,30 @@
+const firebase = require('firebase-admin');
+
+const { serviceAccount, databaseURL } = require('../../../credentials/firebase.json');
+
+firebase.initializeApp({
+    credential: firebase.credential.cert(serviceAccount),
+    databaseURL
+}), 'appCheck';
+
+const appCheck = firebase.appCheck();
+
+module.exports = {
+    async execute({ request, parseError }) {
+        try {
+            const appCheckToken = request.headers['x-firebase-appcheck'];
+
+            if (!appCheckToken) return { appCheckPassed: false };
+
+            try {
+                await appCheck.verifyToken(appCheckToken);
+
+                return { appCheckPassed: true };
+            } catch {
+                return { appCheckPassed: false };
+            }
+        } catch (e) {
+            parseError(e);
+        }
+    }
+}
