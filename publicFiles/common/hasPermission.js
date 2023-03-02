a4bbdb712b2381c3cea2fcec6e9da799e3259055
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-03-02 12:54:05 +0100

    Changed client function to hasPermission

--- publicFiles/common/hasPermission.js
+++ publicFiles/common/hasPermission.js
@@ -2,11 +2,15 @@
 //todo: get all permissions and perform checks on the client side, maybe also create folder with shared code for client and server?
 
 let getAuthHeaders;
-export async function getPermission(permission) {
+export async function hasPermission(permission, checks) {
     if (!getAuthHeaders)
         ({ getAuthHeaders } = (await import('/sdk/auth.js'))._);
 
-    const response = await fetch(`/api/getPermission?permission=${encodeURIComponent(JSON.stringify(permission))}`, {
+    let url = '/api/hasPermission';
+    url += '?permission=' + encodeURIComponent(JSON.stringify(permission));
+    url += '&checks=' + encodeURIComponent(JSON.stringify(checks));
+
+    const response = await fetch(url, {
         headers: {
             ...await getAuthHeaders()
         }
