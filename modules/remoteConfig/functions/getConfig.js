09bb1b5aa778b135cd07640037d35ffc9c12e7ed
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-03-03 13:52:18 +0100

    Implemented remote config cache

--- modules/remoteConfig/functions/getConfig.js
+++ modules/remoteConfig/functions/getConfig.js
@@ -1,37 +1,84 @@
+const settings = require('../../../settings.json')
 const firebase = require('./getFirebase.js');
 const fs = require('fs');
 const path = require('path');
 
-async function getConfig(group) {
-    if (group && fs.existsSync(path.resolve(__dirname, `../cache/group_${group}.json`)))
-        return JSON.parse(fs.readFileSync(path.resolve(__dirname, `../cache/group_${group}.json`)).toString());
-    else if ((!group) && fs.existsSync(path.resolve(__dirname, '../cache/noGroup.json')))
-        return JSON.parse(fs.readFileSync(path.resolve(__dirname, '../cache/noGroup.json')).toString());
+const cacheManifest = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../cache/manifest.json')).toString());
 
-    let template;
+async function getConfigFromCache(group) {
+
+    let cacheFileName;
+
     if (group)
-        template = (await firebase.remoteConfig().getTemplate()).parameterGroups[group].parameters;
+        cacheFileName = `group_${group}.json`;
     else
-        template = (await firebase.remoteConfig().getTemplate()).parameters;
+        cacheFileName = 'noGroup.json';
 
-    if (!template)
+    if (fs.existsSync(path.resolve(__dirname, `../cache/${cacheFileName}`))) {
+        if ((new Date().getTime() - cacheManifest[cacheFileName].age) > (settings.cache.remoteConfig * 60 * 1000))
+            updateCache(group); //don't await so we can return the cached value and update the cache in the background
+
+        return JSON.parse(fs.readFileSync(path.resolve(__dirname, `../cache/${cacheFileName}`)).toString());
+    }
+
+    const config = await getConfig(group);
+
+    fs.writeFileSync(path.resolve(__dirname, `../cache/${cacheFileName}`), JSON.stringify(config));
+    cacheManifest[cacheFileName] = {
+        age: new Date().getTime()
+    };
+    fs.writeFileSync(path.resolve(__dirname, '../cache/manifest.json'), JSON.stringify(cacheManifest));
+
+    return config;
+}
+
+const cacheUpdating = [];
+async function updateCache(group) {
+    console.log('Updating cache', group)
+
+    let cacheFileName;
+
+    if (group)
+        cacheFileName = `group_${group}.json`;
+    else
+        cacheFileName = 'noGroup.json';
+
+    if (cacheUpdating.includes(cacheFileName))
+        return;
+
+    cacheUpdating.push(cacheFileName);
+
+    const config = await getConfig(group);
+
+    fs.writeFileSync(path.resolve(__dirname, `../cache/${cacheFileName}`), JSON.stringify(config));
+    cacheManifest[cacheFileName] = {
+        age: new Date().getTime()
+    };
+    fs.writeFileSync(path.resolve(__dirname, '../cache/manifest.json'), JSON.stringify(cacheManifest));
+
+    cacheUpdating.splice(cacheUpdating.indexOf(cacheFileName), 1);
+}
+
+async function getConfig(group) {
+    let config;
+    if (group)
+        config = (await firebase.remoteConfig().getTemplate()).parameterGroups[group].parameters;
+    else
+        config = (await firebase.remoteConfig().getTemplate()).parameters;
+
+    if (!config)
         return {};
 
-    for (const [key, value] of Object.entries(template)) {
+    for (const [key, value] of Object.entries(config)) {
         if (value.valueType === 'JSON')
-            template[key] = JSON.parse(value.defaultValue?.value);
+            config[key] = JSON.parse(value.defaultValue?.value);
         else if (value.valueType === 'STRING')
-            template[key] = value.defaultValue?.value;
+            config[key] = value.defaultValue?.value;
         else
             throw new Error(`Remote-config valueType ${value.valueType} not implemented`)
     }
 
-    if (group)
-        fs.writeFileSync(path.resolve(__dirname, `../cache/group_${group}.json`), JSON.stringify(template));
-    else
-        fs.writeFileSync(path.resolve(__dirname, '../cache/noGroup.json'), JSON.stringify(template));
-
-    return template;
+    return config;
 }
 
-module.exports = getConfig;
+module.exports = getConfigFromCache;
