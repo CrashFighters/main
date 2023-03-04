a96b7a0e82cecd2fb3d57cb3b2b54534bec63e0f
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-03-04 14:05:37 +0100

    Added trace to performance

--- publicFiles/js/performance.js
+++ publicFiles/js/performance.js
@@ -1,7 +1,26 @@
 import {
-    getPerformance
+    getPerformance,
+    trace
 } from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-performance.js';
 
+let performance;
 export function init(app) {
-    return getPerformance(app);
+    performance = getPerformance(app);
+    return performance;
+}
+
+const traces = {};
+export function startTrace(name) {
+    const t = trace(performance, name);
+    traces[name] = t;
+
+    t.start();
+}
+
+export function stopTrace(name) {
+    const t = traces[name];
+    if (!t) throw new Error(`No trace found with name ${name}`)
+
+    t.stop();
+    delete traces[name];
 }
