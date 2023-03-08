d6cb86c283997b4bab6b29e84712878cf3261f76
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-03-08 21:55:29 +0100

    Created new firebase file

---
+++ publicFiles/common/getHeaders.js
@@ -0,0 +1,17 @@
+/*
+
+//todo-imp: change references to new getHeaders function
+
+--fileRequirements--
+/sdk/auth.js
+/js/appCheck.js
+*/
+
+import { _ } from '/js/appCheck.js';
+
+export const getHeaders = async () => {
+    return {
+        ...await _.getAppCheckHeaders()
+        //todo: why aren't auth headers includes
+    };
+}
