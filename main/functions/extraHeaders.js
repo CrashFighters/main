15695ae1eb5e30e8eea1d1614c64fc823dbc57ed
Oscar <56079903+OscarNOW@users.noreply.github.com>
2023-03-13 19:41:44 +0100

    Added caching of private files

--- main/functions/extraHeaders.js
+++ main/functions/extraHeaders.js
@@ -1,12 +1,13 @@
-const { cache: { publicFiles: { cacheMinutes, staleUseMinutes, errorUseMinutes } } } = require('../../settings.json');
+const { cache: { publicFiles, privateFiles } } = require('../../settings.json');
 
 module.exports = (isPrivate) => {
     const headers = {};
 
-    if (isPrivate)
-        headers['Cache-Control'] = 'private, max-age=0, no-cache, no-store, must-revalidate';
-    else
-        headers['Cache-Control'] = `public, max-age=${cacheMinutes * 60}, stale-while-revalidate=${staleUseMinutes * 60}, stale-if-error=${errorUseMinutes * 60}`
+    if (isPrivate) {
+        headers['Cache-Control'] = `private, max-age=${privateFiles.cacheMinutes * 60}, stale-while-revalidate=${privateFiles.staleUseMinutes * 60}, stale-if-error=${privateFiles.errorUseMinutes * 60}`;
+        headers['Vary'] = 'Cookie';
+    } else
+        headers['Cache-Control'] = `public, max-age=${publicFiles.cacheMinutes * 60}, stale-while-revalidate=${publicFiles.staleUseMinutes * 60}, stale-if-error=${publicFiles.errorUseMinutes * 60}`
 
     return headers;
 }
